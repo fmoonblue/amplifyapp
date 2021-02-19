af245eeac087cae1b9447bd381ebbb82daa395c6
MZC01-LHY <fmoonblue@gmail.com>
2021-02-19 15:48:22 +0900

    add graphQL

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "ap-northeast-2",
     "aws_user_pools_id": "ap-northeast-2_sDc7WXlrE",
     "aws_user_pools_web_client_id": "3cr4shpnidirbktl8bpim0hqp8",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://fkgpntmutrabjdchoct7zopesi.appsync-api.ap-northeast-2.amazonaws.com/graphql",
+    "aws_appsync_region": "ap-northeast-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-sij5ggzmknd4zir627cschaffm"
 };
 
 
